3c8ed0ceb4990f92d2ab47f221fd73ac269abf78
Biozid Bostami <biozid0208@gmail.com>
2020-06-20 22:47:44 +0600

    [Solved] three more

---
+++ DataBases-Arcade/countriesSelection.sql
@@ -0,0 +1,7 @@
+/*Please add ; after each select statement*/
+CREATE PROCEDURE countriesSelection()
+BEGIN
+    select name, continent, population from countries
+    where continent='Africa'
+    order by name;
+END
---
+++ DataBases-Arcade/monthlyScholarships.sql
@@ -0,0 +1,7 @@
+/*Please add ; after each select statement*/
+CREATE PROCEDURE monthlyScholarships()
+BEGIN
+    select id, scholarship/12 as scholarship
+    from  scholarships
+    order by id;
+END
